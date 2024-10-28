277ab2a69d38f3e51bb96d30496eea060bc2243b
teofilo_nicolau <teofilonicolau157@gmail.com>
2024-10-28 11:36:45 -0300

    Initial commit

---
+++ contador-vite/src/App.jsx
@@ -0,0 +1,12 @@
+import './Contador.css';
+import Contador from './Contador';
+
+function App() {
+  return (
+    <div>
+      <Contador />
+    </div>
+  );
+}
+
+export default App;
---
+++ contador-vite/src/Contador.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './Contador.css';
+
+class Contador extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  componentDidMount() {
+    console.log("Componente montado!");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("Contador atualizado!");
+    if (this.state.count % 5 === 0 && this.state.count !== 0) {
+      alert(`Contador atingiu um múltiplo de 5: ${this.state.count}`);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log("Componente será desmontado!");
+  }
+
+  increment = () => {
+    if (this.state.count < 10) {
+      this.setState((prevState) => ({ count: prevState.count + 1 }));
+    }
+  }
+
+  decrement = () => {
+    if (this.state.count > 0) {
+      this.setState((prevState) => ({ count: prevState.count - 1 }));
+    }
+  }
+
+  reset = () => {
+    this.setState({ count: 0 });
+  }
+
+  render() {
+    return (
+      <div className="contador">
+        <h2>Valor do Contador: {this.state.count}</h2>
+        <div className="buttons">
+          <button onClick={this.increment} disabled={this.state.count === 10}>Incrementar</button>
+          <button onClick={this.decrement} disabled={this.state.count === 0}>Decrementar</button>
+          <button onClick={this.reset}>Resetar</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Contador;
